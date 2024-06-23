d2d98d8e6d0f4ef863cd316d176076ada02ad323
Mahdi Mohseni <tty.mohseni@gmail.com>
2024-06-23 16:15:31 +0330

    checker for existing dir(s)

---
+++ taskole/Checkers.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace taskole
+{
+    public static class Checkers
+    {
+        public static bool IsSavingPlaceExist(string path)
+        {
+            if (Directory.Exists(path))
+                return true;
+
+            return false;
+        }
+    }
+}
