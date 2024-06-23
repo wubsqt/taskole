4ef540310acf1807d0dd14ec40990d1e196b018a
Mahdi Mohseni <tty.mohseni@gmail.com>
2024-06-23 16:15:09 +0330

    Add creating directory for saving data

--- taskole/Actions.cs
+++ taskole/Actions.cs
@@ -1,9 +1,23 @@
-﻿namespace taskole
+﻿using System.IO;
+
+namespace taskole
 {
     public static class Actions
     {
         public static void Initial()
         {
+            string _path = "tasks/";
+            string _file = "test1.txt";
+
+            void CreateSavingPlace(string path)
+            {
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+            }
+
+            if (!Checkers.IsSavingPlaceExist(_path))
+                CreateSavingPlace(_path);
+
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.Magenta;
             Console.Write("Taskole Is Running ");
@@ -29,5 +43,6 @@
             Console.WriteLine($"logout on {DateTime.Now} ...");
             Console.ResetColor();
         }
+
     }
 }
